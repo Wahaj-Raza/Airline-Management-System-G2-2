fea3aa12825c085cba53c01e67e2eed0e2c9141b
Wahaj-Raza <wahajraza69@gmail.com>
2023-07-17 00:05:25 +0500

    Altered Amin Page

--- flight/admin.py
+++ flight/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
+admin.AdminSite.site_title = "Admin"
+admin.AdminSite.site_header = "Airline MS DBMS Project G2-2"
+admin.AdminSite.index_title = "Airline MS"
 # Register your models here.
 
 admin.site.register(Place)
